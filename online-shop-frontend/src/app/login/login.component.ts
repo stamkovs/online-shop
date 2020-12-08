7fd25d2000e4bd7303ac68c760cd6b48808a60a4
Stojan Stamkov <stojanstamkov@netcetera.com>
2020-12-08 15:34:04 +0100

    Added login component with styling, lacking functionallity with oauth openid and standard register/login with email verification and jwt or jwe

--- online-shop-frontend/src/app/login/login.component.ts
+++ online-shop-frontend/src/app/login/login.component.ts
@@ -10,6 +10,25 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // ToDo: If time available adapt the below code in angular way perhaps via shared service and observables.
+    const signUpButton = document.getElementById('signUp');
+    const signUpButtonMobile = document.getElementById('signUp-mobile');
+    const signInButton = document.getElementById('signIn');
+    const signInButtonMobile = document.getElementById('signIn-mobile');
+    const container = document.getElementById('login-container');
+
+    signUpButton.addEventListener('click', () =>
+      container.classList.add('right-panel-active'));
+
+    signUpButtonMobile.addEventListener('click', () =>
+      container.classList.add('right-panel-active'));
+
+    signInButton.addEventListener('click', () =>
+      container.classList.remove('right-panel-active'));
+
+    signInButtonMobile.addEventListener('click', () =>
+      container.classList.remove('right-panel-active'));
   }
 
 }
