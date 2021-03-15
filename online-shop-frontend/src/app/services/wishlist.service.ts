95c79393a58e3e8f96e69de9564e8f194e2742ef
Stojan Stamkov <stojanstamkov@netcetera.com>
2021-03-15 20:53:38 +0100

    feat: Committing the wishlist service for contacting the rest endpoints

---
+++ online-shop-frontend/src/app/services/wishlist.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class WishlistService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getWishListProductsForLoggedInUser(): Observable<any> {
+    return this.http.get('rest/get-wishlist');
+  }
+
+  addProductToWishList(productId: string): Observable<any> {
+    return this.http.put('rest/add-to-wishlist', productId);
+  }
+
+  deleteProductFromWishlistById(productId: string): Observable<any> {
+    return this.http.delete(`rest/delete-from-wishlist/${productId}`);
+  }
+}
